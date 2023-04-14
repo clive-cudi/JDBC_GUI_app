acb7db52679e917c4c5f67996055fa9cc07d0058
clive-cudi <clivemaina41@gmail.com>
2023-04-14 10:54:52 +0300

    dynamic-data-from-DB

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,15 +1,65 @@
 package com.company;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.List;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ClassNotFoundException {
         System.out.println("Hello world!");
 
-        // sample user static data [to be removed]
-        User[] users = {new User(1, "Clive", "clivemaina41@gmail.com", 22.00), new User(1,"Mary", "mary@hotmail.com", 98)};
-
+        User[] users = {};
         // render the table in JFrame
         // pull data from DB
 
+        try {
+            // driver init
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            // connection string
+            String db_connection = "jdbc:mysql://localhost/jdbc_gui_users";
+            // connection init
+            Connection connection = DriverManager.getConnection(db_connection, "root", "");
+
+            // statement
+            Statement statement = connection.createStatement();
+
+            // query
+            ResultSet usersResultSet = statement.executeQuery("select * from `users`");
+
+            // iterate through the result set and push each user to list
+            while (usersResultSet.next()) {
+                int[] cols = {1, 2, 3, 4};
+
+                /* [DEBUG] */
+                for(int col: cols) {
+                    System.out.println(usersResultSet.getString(col));
+                }
+                User fetchedUser = new User(usersResultSet.getInt(1), usersResultSet.getString(2), usersResultSet.getString(3), usersResultSet.getDouble(4));
+
+                users = addToUsers(users, fetchedUser);
+            };
+
+            System.out.println("Database connection successful!!");
+        } catch (Exception e) {
+            System.out.println("An Error occurred while connecting to DB!");
+            System.out.println(e);
+        }
+
         new AppFrame(users);
+    }
+
+    private static User[] addToUsers(User[] initialUsers, User user) {
+        User[] newArr = new User[initialUsers.length + 1];
+
+        for (int i = 0; i < initialUsers.length; i++) {
+            newArr[i] = initialUsers[i];
+        }
+
+        newArr[initialUsers.length]  = user;
+
+        return newArr;
     }
 }
